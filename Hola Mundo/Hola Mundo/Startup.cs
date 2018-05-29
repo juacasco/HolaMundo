bf467422b58b35a47371b1cef7f65d2e1cbc36c0
Juan Pablo Carrasco Venegas <juan.carrasco@hp.com>
2018-05-28 22:11:34 -0500

    Commit Project

---
+++ Hola Mundo/Hola Mundo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hola_Mundo.Startup))]
+namespace Hola_Mundo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
